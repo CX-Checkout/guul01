0bb4abf0d75873c9de8a4a0e2a24fd92fd2b1e0a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-29 15:51:56 +0000

    Sun Oct 29 15:51:56 GMT 2017

--- lib/solutions/checkout.ts
+++ lib/solutions/checkout.ts
@@ -5,4 +5,10 @@ export function checkout(skus: string[]) {
     if (!Array.isArray(skus)) {
         return -1;
     }
+
+    for (let i = 0; i < skus.length; i++) {
+        if (typeof skus[i] !== 'string') {
+            return -1;
+        }
+    }
 }
---
+++ lib/solutions/items.ts
@@ -0,0 +1,17 @@
+
+
+
+export var items = {
+    "A": {
+        price: 50
+    },
+    "B": {
+        price: 30
+    },
+    "C": {
+        price: 20
+    },
+    "D": {
+        price: 15
+    }
+};
